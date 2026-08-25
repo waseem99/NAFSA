d61f2251d97455a39eda0ec93e17e2dadb472d57
waseem99 <waseem99@users.noreply.github.com>
2026-08-25 23:38:51 +0500

    Add regulatory search component

---
+++ prototype/components/search/RegulatorySearch.tsx
@@ -0,0 +1,10 @@
+export default function RegulatorySearch() {
+  return (
+    <div className="rounded-lg border p-4">
+      <input
+        className="w-full"
+        placeholder="Search regulations, requirements, laboratories and alerts"
+      />
+    </div>
+  );
+}
